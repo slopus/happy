2b62ce3ca2730b3864648d4bb43de05791f598dd
Kirill Dubovitskiy <kirill2003de@gmail.com>
2025-07-22 22:37:08 -0700

    chore: fixup scripts that disappeared

--- app/metro.config.js
+++ app/metro.config.js
@@ -4,6 +4,20 @@ const path = require("path");
 
 const config = getDefaultConfig(__dirname);
 
+// Define paths
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, "../");
+
+const liberalShared = path.resolve(monorepoRoot, "liberal");
+
+const ourModules = path.resolve(projectRoot, "node_modules");
+
+// Set up watch folders - Metro needs to watch all workspace directories
+config.watchFolders = [projectRoot, liberalShared];
+
+// Configure module resolution - tell Metro where to find node_modules
+config.resolver.nodeModulesPaths = [liberalShared, ourModules];
+
 module.exports = withNativeWind(config, {
   input: path.join(__dirname, "sources/global.css"),
 });
