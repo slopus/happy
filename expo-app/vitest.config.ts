d97924f3ef398638746919a3fee3675b0a12cee6
Leeroy Brun <leeroy.brun@gmail.com>
2026-01-18 22:16:09 +0100

    chore(test): define __DEV__ for vitest

--- expo-app/vitest.config.ts
+++ expo-app/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 import { resolve } from 'node:path'
 
 export default defineConfig({
+    define: {
+        __DEV__: false,
+    },
     test: {
         globals: false,
         environment: 'node',
