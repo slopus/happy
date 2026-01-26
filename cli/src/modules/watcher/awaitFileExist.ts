eab155dfca9cdb2883d8d16e906c1f6b07325772
Leeroy Brun <leeroy.brun@gmail.com>
2026-01-26 15:49:04 +0100

    chore(structure-cli): P3-CLI-3 integrations bucket

--- cli/src/modules/watcher/awaitFileExist.ts
+++ cli/src/modules/watcher/awaitFileExist.ts
@@ -1,15 +1,2 @@
-import { access } from "fs/promises";
-import { delay } from "@/utils/time";
+export * from '@/integrations/watcher/awaitFileExist';
 
-export async function awaitFileExist(file: string, timeout: number = 10000) {
-    const startTime = Date.now();
-    while (Date.now() - startTime < timeout) {
-        try {
-            await access(file);
-            return true;
-        } catch (e) {
-            await delay(1000);
-        }
-    }
-    return false;
-}
