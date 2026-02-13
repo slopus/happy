3180047a7baecaddc8dc79ca105c97d88b4b1a97
Steve Korshakov <steve@korshakov.com>
2026-02-12 20:58:09 -0800

    Implement v3 reliable HTTP messages API

--- packages/happy-server/sources/app/api/api.ts
+++ packages/happy-server/sources/app/api/api.ts
@@ -21,6 +21,7 @@ import { enableAuthentication } from "./utils/enableAuthentication";
 import { userRoutes } from "./routes/userRoutes";
 import { feedRoutes } from "./routes/feedRoutes";
 import { kvRoutes } from "./routes/kvRoutes";
+import { v3SessionRoutes } from "./routes/v3SessionRoutes";
 
 export async function startApi() {
 
@@ -66,6 +67,7 @@ export async function startApi() {
     userRoutes(typed);
     feedRoutes(typed);
     kvRoutes(typed);
+    v3SessionRoutes(typed);
 
     // Start HTTP 
     const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3005;
